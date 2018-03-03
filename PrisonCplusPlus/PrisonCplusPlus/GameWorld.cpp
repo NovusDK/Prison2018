b3d6b399776ee56e294099609abff94942685ae1
Daniel <lykke.daniel@hotmail.dk>
2018-03-03 13:46:48 +0100

    wtf

--- PrisonCplusPlus/PrisonCplusPlus/GameWorld.cpp
+++ PrisonCplusPlus/PrisonCplusPlus/GameWorld.cpp
@@ -1,6 +1,14 @@
 #include "GameWorld.h"
-#include <GLFW\glfw3.h>
 
+void GameWorld::GameLoop()
+{
+	GameLogic();
+	Render();
+	glfwPollEvents();
+}
+
+void GameWorld::GameLogic()
+{
 
 GameWorld::GameWorld(GLFWwindow * windowContext)
 {
@@ -19,31 +27,30 @@ void GameWorld::GameLoop()
 	Render();
 	glfwPollEvents();
 }
-
-void GameWorld::GameLogic()
-{
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) //Tjekker op på at ESC er trykket ned, hvis ja luk vinduet
 	{
 		glfwSetWindowShouldClose(window, true);
 	}
-	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-	{
-		gameObject->direction = -1; // Gå til venstre
-		gameObject->Update();
-	}
-	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-	{
-		gameObject->direction = 1; // Gå til højre
-		gameObject->Update();
-	}
-	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-	{
-		gameObject->direction = 1;
-		gameObject->Update();
-	}
+	//if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+	//{
+	//	direction = -1; // Gå til venstre
+	//}
+	//if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+	//{
+	//	direction = 1; // Gå til højre
+	//}
+	//if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+	//{
+
+	//}
+
+
+void GameWorld::GameLogic()
+{
+	gameObject->Update();
 }
 
-void GameWorld::Render()
+GameWorld::~GameWorld()
 {
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
