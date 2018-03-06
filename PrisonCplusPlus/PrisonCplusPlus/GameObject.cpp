758cf53931f7d889c33343f0fe7a30a8c1f21daf
S Krogshede <novus@outlook.dk>
2018-03-06 09:56:29 +0100

    Speed

--- PrisonCplusPlus/PrisonCplusPlus/GameObject.cpp
+++ PrisonCplusPlus/PrisonCplusPlus/GameObject.cpp
@@ -5,14 +5,15 @@
 
 GameObject::GameObject() : GameObject(health, speed, 0,0,0)
 {
-	health = 10;
-	speed = 0.00001;
+	health;
+	speed = 0.03;
+	direction = 1;
 }
 
 GameObject::GameObject(int health, float speed, float initX, float initY, float initZ)
 {
 	health = 10;
-	speed = 0.00001;
+	speed;
 	x = initX;
 	y = initY;
 	z = initZ;
@@ -27,7 +28,7 @@ GameObject::~GameObject()
 
 void GameObject::Update()
 {
-	x += speed;
+	x += direction * speed; // Her kan vi stille hastighed på piletasterne
 }
 
 void GameObject::Render()
@@ -35,7 +36,8 @@ void GameObject::Render()
 	glPushMatrix(); //Lægger en matrise på stakken, således kun denne manipuleres
 	glTranslatef(x, y, z);
 	glBegin(GL_TRIANGLES);
-	glColor3f(0.0f, 0.0f, 0.0f);
+	
+	//glColor3f(1.0f, 0.0f, 0.0f);
 	glVertex3f(-20.0f, 0.0f, 0.0f);
 	glVertex3f(0.0f, 20.0f, 0.0f);
 	glVertex3f(20.0f, 0.0f, 0.0f);
